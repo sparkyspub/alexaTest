388029449a87e4458dcd26e3828600138e14d8bc
Jorge Carmona <jcarmonausf@gmail.com>
2018-05-10 16:01:35 -0500

    added node mods and index

---
+++ index.js
@@ -0,0 +1,201 @@
+'use strict';
+
+var winston = require('winston');
+// Formatting testing outputs
+var logger = new (winston.Logger)({
+  transports: [
+    new(winston.transports.Console)({ prettyPrint: true, timestamp: true, json: false, stderrLevels:['error'] })
+  ]
+});
+// Object placement for intents
+var intentHandlers = {};
+// Testing environment
+if(process.env.NODE_DEBUG_EN) {
+  logger.level = 'debug';
+}
+
+exports.handler = function (event, context) {
+  try {
+    logger.info('event.session.application.applicationId=' + event.session.application.applicationId);
+
+    if(APP_ID !== '' && event.session.application.applicationId !== APP_ID) {
+      context.fail('Invalid Application ID');
+    }
+    if(!event.session.attributes) {
+      event.session.attributes = {};
+    }
+// Outputs in console
+    logger.debug('Incoming request:\n', JSON.stringify(event,null,2));
+// Starts session event
+    if(event.session.new) {
+      onSessionStarted({requestId: event.request.requestId}, event.session);
+    }
+// Requests that open, utilize, and close the skill
+    if(event.request.type === 'LaunchRequest') {
+      onLaunch(event.request, event.session, new Response(context,event.session));
+    } else if (event.request.type === 'IntentRequest') {
+      var response = new Response(context,event.session);
+      if(event.request.intent.name in intentHandlers) {
+        intentHandlers[event.request.intent.name](event.request, event.session, response,getSlots(event.request));
+      } else {
+        response.speechText = 'Unknown intent';
+        response.shouldEndSession = true;
+        response.done();
+      }
+    } else if (event.request.type === 'SessionEndedRequest') {
+      onSessionEnded(event.request, event.session);
+      context.succeed();
+    }
+  } catch (e) {
+    context.fail('Exception: ' + getError(e));
+  }
+};
+// Variable slots used for the skill i.e FirstName
+function getSlots(req) {
+  var slots = {};
+  for(var key in req.intent.slots) {
+    slots[key] = req.intent.slots[key].value;
+  }
+  return slots;
+}
+
+var Response = function (context, session) {
+  // variable to hold what is said by Alexa
+  this.speechText = '';
+  // variable to close the skill
+  this.shouldEndSession = true;
+  // variable for helper speech functionality
+  this.ssmlEn = true;
+  // local variables; limited scope
+  this._context = context;
+  this._session = session;
+// Simple response function
+  this.done = function(options) {
+    if(options && options.speechText) {
+      this.speechText = options.speechText;
+    }
+    if(options && options.repromptText) {
+      this.repromptText = options.ssmlEn;
+    }
+    if(options && options.shouldEndSession) {
+      this.shouldEndSession = options.shouldEndSession;
+    }
+
+    this._context.succeed.(buildAlexResponse(this.));
+  }
+
+  this.fail = function(msg) {
+    logger.error(msg);
+    this._context.fail(msg);
+  }
+};
+// Speech Synthesis Markup Language
+function createSpeechObject(text,ssmlEn) {
+  if(ssmlEn) {
+    return {
+      type: 'SSML',
+      ssml: '<speak>'+text+'</speak>'
+    }
+  } else {
+    return {
+      type: 'PlainText',
+      text: text
+    }
+  }
+}
+// Object sent to respond user
+function buildAlexaResponse(response) {
+  var alexaResponse = {
+    version: '1.0',
+    response: {
+      outputSpeech: createSpeechObject(response.speechText,response.ssmlEn),
+      shouldEndSession: response.shouldEndSession
+    }
+  };
+// Replying to user
+  if(response.repromptText) {
+    alexaResponse.response.reprompt = {
+      outputSpeech: createSpeechObject(response.repromptText,response.ssmlEn)
+    };
+  }
+// Card for Alexa Show
+  if(response.cardTitle) {
+    alexaResponse.response.card = {
+      type: 'Simple',
+      title: response.cardTitle
+    };
+// Image for Alex Show
+    if(response.imageUrl) {
+      alexaResponse.response.card.type = 'Standard';
+      alexaResponse.response.card.text = response.cardContent;
+      alexaResponse.response.card.image = {
+        smallImageUrl: response.imageUrl,
+        largeImageUrl: response.imageUrl
+      };
+    } else {
+      alexaResponse.response.card.content = response.cardContent;
+    }
+  }
+
+  if (!response.shouldEndSession && response._session && response._session.attributes) {
+    alexaResponse.sessionAttributes = response._session.attributes;
+  }
+  logger.debug('Final response:\n', JSON.stringify(alexaResponse,null,2),'\n\n');
+  return alexaResponse;
+}
+// Formatting error readability
+function getError(err) {
+  var msg='';
+  if (typeof err === 'object') {
+    if (err.message) {
+      msg = ': Message : ' + err.message;
+    }
+    if (err.stack) {
+      msg += '\nStacktrace:';
+      msg += '\n====================\n';
+      msg += err.stack;
+    }
+  } else {
+    msg = err;
+    msg += ' - This error is not object';
+  }
+  return msg;
+}
+
+
+//--------------------------------------------- Skill specific logic starts here -----------------------------------------
+
+//Add your skill application ID from amazon devloper portal
+var APP_ID = '';
+
+function onSessionStarted(sessionStartedRequest, session) {
+    logger.debug('onSessionStarted requestId=' + sessionStartedRequest.requestId + ', sessionId=' + session.sessionId);
+    // add any session init logic here
+
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+  logger.debug('onSessionEnded requestId=' + sessionEndedRequest.requestId + ', sessionId=' + session.sessionId);
+  // Add any cleanup logic here
+
+}
+
+function onLaunch(launchRequest, session, response) {
+  logger.debug('onLaunch requestId=' + launchRequest.requestId + ', sessionId=' + session.sessionId);
+
+  //response.speechText = 'Welcome msg';
+  //response.repromptText = 'Reprompt msg';
+  response.shouldEndSession = false;
+  response.done();
+}
+
+
+/** For each intent write a intentHandlers
+Example:
+intentHandlers['HelloIntent'] = function(request,session,response,slots) {
+  //Intent logic
+
+}
+
+----------Will add additional functionality for Alexa memory and In-app purchases for V2--------------
+**/
